ad3c666cb7c07ff4be6c1e489486734cd681df8e
syaripuddin <syaripuddin.digitalent>
2020-10-14 20:39:33 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import router from './router.js';
+
+const app = express();
+
+// Connect to DB
+mongoose.connect(process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }, () => {
+  console.log('Connect to database success');
+});
+
+// Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req, res, next) => {
+  res.json({
+    message: 'success',
+  });
+});
+
+app.use('/api', router);
+
+const PORT = process.env.PORT || '4000'
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listens to port ${process.env.PORT}`);
+});
